aa9d6dc6fb5baaa40a41d3eedd31bd7023bb0001
MiritoSun <87271458+MichaelSamarati@users.noreply.github.com>
2023-04-25 20:15:18 +0200

    🫡

--- src/sql/trash.sql
+++
@@ -1,9 +0,0 @@
-🍔🍕🥑
-
--- CREATE DATABASE database_name DEFAULT CHARSET = utf8mb4 DEFAULT COLLATE = utf8mb4_unicode_ci;
-
-/*SET character_set_connection = 'utf8mb4';*/
-
--- CREATE DATABASE IF NOT EXISTS classcircle
--- 	CHARACTER SET = 'utf8'
--- 	COLLATE 'utf8_german2_ci';
