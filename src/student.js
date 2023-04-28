799f83a53a82372a673d5bac1134f14272686642
MiritoSun <87271458+MichaelSamarati@users.noreply.github.com>
2023-04-28 20:06:37 +0200

    🫠

--- src/student.js
+++ src/student.js
@@ -3,9 +3,8 @@ const file = require("./file");
 async function exchangeImagenameWithImage(student) {
   try {
     student.image = await file.getBase64FromImageFile(student.imageName);
-    await delete student.imageName;
+    delete student.imageName;
   } catch (e) {}
-  console.log("studnet", student.firstname);
   return student;
 }
 
